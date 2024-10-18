9a65a0179091f75265a6f3180c1c2fd2138e5d60
stancillous <stancillousray@gmail.com>
2024-10-18 15:08:46 +0300

    change whole look

--- index.js
+++ index.js
@@ -2,11 +2,33 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
   gsap.registerPlugin(ScrollTrigger);
   pageFunctions()
+  faqs()
   // dynamically set year on the footer
   document.querySelector(".year-wrp").textContent =  new Date().getFullYear()
 });
 
 
+// function for the 'faq' section
+function faqs(){
+       let question = document.querySelectorAll(".question");
+
+       question.forEach((question) => {
+         question.addEventListener("click", (event) => {
+           const active = document.querySelector(".question.active");
+           if (active && active !== question) {
+             active.classList.toggle("active");
+             active.nextElementSibling.style.maxHeight = 0;
+           }
+           question.classList.toggle("active");
+           const answer = question.nextElementSibling;
+           if (question.classList.contains("active")) {
+             answer.style.maxHeight = answer.scrollHeight + "px";
+           } else {
+             answer.style.maxHeight = 0;
+           }
+         });
+       });
+}
 
 function pageFunctions() {
 
@@ -32,21 +54,37 @@ function pageFunctions() {
         ease: "Back.easeOut",
     })
 
+    /**animation for the part that has info that look like FAQs */
+     let dashboardFeaturesTl = gsap.timeline({
+       scrollTrigger: {
+         trigger: ".faq-section",
+         start: "top center",
+         ease: "Back.easeOut",
+         toggleActions: "play pause resume reset",
+       },
+     });
+
+     dashboardFeaturesTl.from(".faq-wrp", {
+      opacity: 0,
+      y: 34
+     })
+
+    /**animation for the image on the hero section */
     let imgTl = gsap.timeline({
       scrollTrigger: {
         trigger: ".lt-benefits-wrapper",
-        // markers: true,
         scrub: 1,
-        start: "top center",
+        start: "top 60%",
         ease: "Back.easeOut",
         toggleActions: "play pause resume reset",
       },
     });
 
-    imgTl.to(".tps-img", {
+    imgTl.to(".person-img", {
         x: 232,
         scale: 4
     })
+    /**animation for the plans/pricing section */
     let plansTl = gsap.timeline({
       scrollTrigger: {
         trigger: ".available-plans-section",
@@ -60,6 +98,7 @@ function pageFunctions() {
     plansTl.from(".plans-animation", {y: 12, opacity: 0, stagger: .06})
     plansTl.from(".apw-plan", {y: 6, opacity: 0, stagger: .06})
     
+    /**animation for the image showing different payment methods/features */
     let mtkTl = gsap.timeline({
       scrollTrigger: {
         trigger: ".mtk",
@@ -80,6 +119,7 @@ function pageFunctions() {
         opacity: 0,
     })
     
+    /**animation for the footer */
     let footerTl = gsap.timeline({
       scrollTrigger: {
         trigger: ".footer-section-wrapper",
